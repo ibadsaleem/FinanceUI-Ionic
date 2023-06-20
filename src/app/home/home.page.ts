b738f7d2e44dd15601a4157f2458d0d275faf8af
IBADSALEEM\IbadSaleem <ibad.saleem@adalfi.com>
2023-06-20 14:56:09 +0500

    Ionic Finance UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  features: any[] = [
+    {id: 1, name: 'Top Up', src: 'assets/icons/top-up.png', background: 'rgba(27,150,181, 0.1)', page: ''},
+    {id: 2, name: 'Withdraw', src: 'assets/icons/cash-withdrawal.png', background: 'rgba(106,100,255, 0.1)', page: ''},
+    {id: 3, name: 'Send', src: 'assets/icons/send.png', background: 'rgba(255, 196, 9, 0.1)', page: ''},
+    {id: 4, name: 'Pay', src: 'assets/icons/debit-card.png', background: 'rgba(27,150,181, 0.1)', page: ''},
+  ];
 
-  constructor() {}
+  transactions: any[] = [
+    {id: 1, vendor: 'David', image: '', amount: 1500, time: '13:00'},
+    {id: 2, vendor: 'John', image: '', amount: -1200, time: '14:00'}
+  ];  constructor() {}
 
+  func=()=>{
+    alert('Hello')
+  }
 }
